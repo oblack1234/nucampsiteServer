43a38abc3413669047b32b586eb1c9fcf2a449bf
oblack1234 <fireslash98@gmail.com>
2022-01-12 08:47:48 -0500

    Add files via upload

--- authenticate.js
+++ authenticate.js
@@ -3,7 +3,9 @@ const LocalStrategy = require('passport-local').Strategy;
 const User = require('./models/user');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const FacebookTokenStrategy = require('passport-facebook-token');
 const jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
+const createError = require('http-errors')
 
 const config = require('./config.js');
 
@@ -37,14 +39,46 @@ exports.jwtPassport = passport.use(
     )
 );
 
+exports.facebookPassport = passport.use(
+    new FacebookTokenStrategy(
+        {
+            clientID: config.facebook.clientId,
+            clientSecret: config.facebook.clientSecret
+        }, 
+        (accessToken, refreshToken, profile, done) => {
+            User.findOne({facebookId: profile.id}, (err, user) => {
+                if (err) {
+                    return done(err, false);
+                }
+                if (!err && user) {
+                    return done(null, user);
+                } else {
+                    user = new User({ username: profile.displayName });
+                    user.facebookId = profile.id;
+                    user.firstname = profile.name.givenName;
+                    user.lastname = profile.name.familyName;
+                    user.save((err, user) => {
+                        if (err) {
+                            return done(err, false);
+                        } else {
+                            return done(null, user);
+                        }
+                    });
+                }
+            });
+        }
+    )
+);
+
 exports.verifyUser = passport.authenticate('jwt', {session: false});
 
-exports.verifyAdmin = (req, res, next) => {
+exports.verifyAdmin = ((req, res, next) => {
     if (req.user.admin) {
-        return next();
+        return next()
     } else {
-        const err = new Error('You are not authorized to perform this operation');
-        err.status = 403;
-        return next(err);
+        const err = new Error("You are not authorized to perform this operation!"
+        )
+        err.status = 403 
+        return next(err)
     }
-}
+})
--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '190877199879291',
+        clientSecret: '92f26415088e692fc4d7a9374fd497c4'
+    }
 }
